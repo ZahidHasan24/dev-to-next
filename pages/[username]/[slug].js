c279e21eebf6fa438f0aef8f7163e477beec8d08
zahid hasan <hzahid2999@gmail.com>
2021-06-25 16:35:39 +0600

    implemented single post page functionality

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,9 +1,70 @@
+import styles from "../../styles/Post.module.css";
+import PostContent from "../../components/PostContent";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
 const Post = () => {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
   return (
-    <main>
-      <h1>Post</h1>
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+      </aside>
     </main>
   );
 };
 
 export default Post;
+
+export const getStaticProps = async ({ params }) => {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+};
+
+export const getStaticPaths = async () => {
+  // Improve my using Admin SDK to select emepty docs
+  const snapshot = await firestore.collectionGroup("posts").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: {
+        username,
+        slug,
+      },
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths: [
+    //  { params: { username, slug}}
+    // ]
+    paths,
+    fallback: "blocking",
+  };
+};
