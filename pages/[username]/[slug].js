ee5c1a9ed26eacf6c1338deb3171fc3c1b8a7124
zahid hasan <hzahid2999@gmail.com>
2021-07-03 19:41:16 +0600

    implemented like dislike functionality

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -2,12 +2,20 @@ import styles from "../../styles/Post.module.css";
 import PostContent from "../../components/PostContent";
 import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import AuthCheck from "../../components/AuthCheck";
+import HeartButton from "../../components/HeartButton";
+import { useContext } from "react";
+import Link from "next/link";
+import { UserContext } from "../../lib/context";
 
 const Post = (props) => {
   const postRef = firestore.doc(props.path);
+  //console.log({ postRef });
   const [realtimePost] = useDocumentData(postRef);
 
   const post = realtimePost || props.post;
+
+  const { user: currentUser } = useContext(UserContext);
 
   return (
     <main className={styles.container}>
@@ -18,6 +26,20 @@ const Post = (props) => {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
       </aside>
     </main>
   );
